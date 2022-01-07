71b855f6679f70f32f4f57e3ef43fe9644d9b12c
santamayengb <84440714+santamayengb@users.noreply.github.com>
2022-01-07 11:41:13 +0530

    login app using strapi endpoint

--- lib/services/chopper_api.dart
+++ lib/services/chopper_api.dart
@@ -1,4 +1,6 @@
 import 'package:chopper/chopper.dart';
 
 @ChopperApi()
-abstract class ApiService extends ChopperService {}
+abstract class ApiService extends ChopperService {
+  static ApiService create({ChopperClient? client}) => _$ApiService(client);
+}
